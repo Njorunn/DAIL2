aca1722664729f22f36cd8880b959ac2ad79f6c4
Sean Becker <kathanious@gmail.com>
2016-11-06 20:06:47 -0600

    Initial Population
    Here it comes.

---
+++ DAIL - Working/Modes/GQ_Merge_Data/updatearcs.bat
@@ -0,0 +1,7 @@
+archivetool Effects.arc -update . Effects 6
+archivetool Creatures.arc -update . Creatures 6
+archivetool Items.arc -update . Items 6
+archivetool xpack.arc -update . xpack 6
+archivetool drx.arc -update . drx 6
+archivetool drxtextures.arc -update . drxtextures 6
+@pause
